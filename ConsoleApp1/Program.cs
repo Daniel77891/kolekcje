4906572196923ff7ca19626cc62d3e4c2c97ffd5
Technikum <Technikum@LAPTOP-IKGJID1K>
2026-09-21 13:32:16 +0200

    losowanie listy bez powtorzzeń

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -35,3 +35,31 @@ int[] wczytajLiczbyDoTablicy(int ileLiczb)
 }
 int[] tablicaWczytana = wczytajLiczbyDoTablicy(6);
 wypiszTablice(tablicaWczytana);
+
+List<int>wylosowanieliczbbezpowtorzen(int ileLiczb)
+{
+    List<int> liczbaLiczbLosowych = new List<int>();
+    //w listach mozna zmienac rozmiar
+    //w trakcie działań programu
+    Random ranndom = new Random();
+    for(int i =0; i < ileLiczb; i++)
+    {
+        int liczba = ranndom.Next(1, 10);
+            while (liczbaLiczbLosowych.Contains(liczba)){
+        liczba = ranndom.Next(1, 10);
+    }
+        liczbaLiczbLosowych.Add(liczba);
+    }
+    return liczbaLiczbLosowych;
+}
+void wypiszListę(List<int> listaLiczb)
+{
+    Console.WriteLine();
+    Console.WriteLine("lista");
+    for(int i = 0; i < listaLiczb.Count; i++)
+    {
+        Console.Write(listaLiczb[i] + ", ");
+    }
+}
+List<int> wylosowane = wylosowanieliczbbezpowtorzen(7);
+wypiszListę(wylosowane);
