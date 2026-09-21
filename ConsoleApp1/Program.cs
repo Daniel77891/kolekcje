9365778bc2dfacadc039fa0e9f647a1752a01fc7
Technikum <Technikum@LAPTOP-IKGJID1K>
2026-09-21 12:36:54 +0200

    wylosowanie i wypisanie tasblicy

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,25 @@
 ﻿Console.WriteLine("Hello, World!");
+//tablica 6 wartości losowych
+//funkcja zwraca tablice przekazujemy do niej liczbe elementow
+
+int[] WylosujTablice(int LiczbaElementów)
+{
+    int[] tablicaWylosowana = new int[LiczbaElementów];
+    for(int i = 0; i < tablicaWylosowana.Length; i++)
+    {
+        Random random = new Random();
+        tablicaWylosowana[i] = random.Next(1, 100);
+    }
+    return tablicaWylosowana;
+}
+void wypiszTablice(int[] tablicaDoWypisywania)
+{
+    Console.WriteLine("tablica");
+    for (int i = 0; i < tablicaDoWypisywania.Length; i++)
+    {
+        Console.Write(tablicaDoWypisywania[i] + ", ");
+    }
+}
+
+int[] tablicaTestowa = WylosujTablice(10);
+wypiszTablice(tablicaTestowa);
